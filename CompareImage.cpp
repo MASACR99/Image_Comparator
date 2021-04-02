dd53a8d3e506a5880387bdbe34a7c87bb321282d
MASACR99 <joangil99@gmail.com>
2021-04-02 18:57:59 +0200

    Implemented some of kiwirives ideas
    Basically warning and code cleanup

--- CompareImage.cpp
+++ CompareImage.cpp
@@ -25,6 +25,76 @@ std::vector<path> image_path; //Vector of paths in which to store all paths to t
 std::vector<path> repeated; //Vector in which to store all paths to REPEATED images
 int progress; //Used for progress bar shit
 
+//Method to delete the repeated images, if option is true it will delete based on rows and cols, if it's false it will delete based on file size
+void removeImages(bool option)
+{
+	//Check size and delete lower
+	int size1;
+	int size2;
+	std::cout << "Starting deletion of images\n";
+	for (int i = 0; i < repeated.size(); i = i + 2)
+	{
+		if (option)
+		{
+			size1 = file_size(repeated[i]);
+			size2 = file_size(repeated[i + 1]);
+		}
+		else
+		{
+			cv::UMat image1 = cv::imread(repeated[i].string(), cv::IMREAD_UNCHANGED).getUMat(cv::ACCESS_READ);
+			cv::UMat image2 = cv::imread(repeated[i].string(), cv::IMREAD_UNCHANGED).getUMat(cv::ACCESS_READ);
+			size1 = image1.cols + image1.rows;
+			size2 = image2.cols + image2.rows;
+		}
+		if (size1 < size2)
+		{
+			remove(repeated[i]);
+		}
+		else
+		{
+			remove(repeated[i + 1]);
+		}
+	}
+	std::cout << "Finished removal \n";
+}
+
+void manualDelete()
+{
+	//Start showing every pair of images and ask Right (R) or Left(L)
+	char choice;
+	std::cout << "Showing name of images, choose using L (left) or R (right), any other character will skip it, use Ctrl+C to skip all";
+	for (int i = 0; i < repeated.size(); i = i + 2)
+	{
+		std::cout << "L= " << repeated[i] << "of size: " << file_size(repeated[i]) << "\n"
+			<< "  R= " << repeated[i + 1] << "of size: " << file_size(repeated[i + 1]);
+		std::cin >> choice;
+		choice = std::tolower(choice);
+		switch (choice)
+		{
+		case 'l':
+			remove(repeated[i]);
+			break;
+		case 'r':
+			remove(repeated[i + 1]);
+			break;
+		default:
+			std::cout << "Wrong char, no image was deleted";
+			break;
+		}
+		std::cout << "\n";
+	}
+}
+
+bool isImage(std::string extension) {
+	if ((extension.compare(".jpg")) == 0 || (extension.compare(".jpeg")) == 0 || (extension.compare(".png")) == 0 || (extension.compare(".webp")) == 0 || (extension.compare(".tiff")) == 0 || (extension.compare(".tif")) == 0)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 //Method receives path, searches all images, loads vector and does anything with the images (delete or show)
 void search(const std::string& searc_path, const int options)
 {
@@ -35,7 +105,7 @@ void search(const std::string& searc_path, const int options)
 	auto start = std::chrono::high_resolution_clock::now(); //Just to know how long the program execution took, starting... now!
 	//Redirect all opencv errors, there's a shitty one that doesn't affect execution but prompts sometimes
 	cv::redirectError(handleError);
-	freopen(NULLSTREAM, "w", stderr);
+	auto suppress_error = freopen(NULLSTREAM, "w", stderr);
 	//Search for all the files and directories inside the firstly specified path
 	std::cout << "Looking for images in folders and loading them...\n";
 	for (const auto& el : recursive_directory_iterator(searc_path, directory_options::skip_permission_denied))
@@ -43,7 +113,7 @@ void search(const std::string& searc_path, const int options)
 		if (is_directory(el) != true)
 		{
 			aux = el.path().extension().string();
-			if ((aux.compare(".jpg")) == 0 || (aux.compare(".jpeg")) == 0 || (aux.compare(".png")) == 0 || (aux.compare(".webp")) == 0  || (aux.compare(".tiff")) == 0 ||(aux.compare(".tif")) == 0)
+			if (isImage(aux))
 			{
 				image_path.push_back(el.path());
 			}
@@ -62,9 +132,7 @@ void search(const std::string& searc_path, const int options)
 	int step = 1;
 	int displayNext = step;
 	int percent = 0;
-
-	for (; progress < image_path.size();)
-	{
+	do {
 		//Lower CPU usage for this bad boi, goes at start because it would skip last iteration if put at end
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		percent = ((100 * (progress + 1)) / image_path.size());
@@ -73,12 +141,12 @@ void search(const std::string& searc_path, const int options)
 		}
 		if (percent >= displayNext)
 		{
-			std::cout << "\r" << "[" << std::string(percent / 5, (char)254u) << std::string((int) (100 / 5 - percent / 5), ' ') << "]";
+			std::cout << "\r" << "[" << std::string(percent / 5, (char)254u) << std::string((int)(100 / 5 - percent / 5), ' ') << "]";
 			std::cout << percent << "%" << " [Image " << progress << " of " << image_path.size() << "]";
 			std::cout.flush();
 			displayNext += step;
 		}
-	}
+	} while (progress < image_path.size());
 
 	//Wait for all threads to end
 	//this code isn't that important, since all threads are gonna end before getting here due to the
@@ -92,62 +160,13 @@ void search(const std::string& searc_path, const int options)
 	switch (options)
 	{
 	case 1:
-		//Check size and delete lower
-		std::cout << "Starting deletion of images\n";
-		for (int i = 0; i < repeated.size(); i = i + 2)
-		{
-			if ((file_size(repeated[i])) < (file_size(repeated[i + 1])))
-			{
-				remove(repeated[i]);
-			}
-			else
-			{
-				remove(repeated[i + 1]);
-			}
-		}
-		std::cout << "Finished removal \n";
+		removeImages(false); //false: based on size
 		break;
 	case 2:
-		std::cout << "Starting deletion of images\n";
-		for (int i = 0; i < repeated.size(); i = i + 2)
-		{
-			cv::UMat image1 = cv::imread(repeated[i].string(), cv::IMREAD_UNCHANGED).getUMat(cv::ACCESS_READ);
-			cv::UMat image2 = cv::imread(repeated[i].string(), cv::IMREAD_UNCHANGED).getUMat(cv::ACCESS_READ);
-			if ((image1.cols + image1.rows) < (image2.cols + image2.rows))
-			{
-				remove(repeated[i]);
-			}
-			else
-			{
-				remove(repeated[i + 1]);
-			}
-		}
-		std::cout << "Finished removal\n";
+		removeImages(true); //true: based on rows adn cols
 		break;
 	case 3:
-		//Start showing every pair of images and ask Right (R) or Left(L)
-		char choice;
-		std::cout << "Showing name of images, choose using L (left) or R (right), any other character will skip it, use Ctrl+C to skip all";
-		for (int i = 0; i < repeated.size(); i = i + 2)
-		{
-			std::cout << "L= " << repeated[i] << "of size: " << file_size(repeated[i]) << "\n"
-				<< "  R= " << repeated[i + 1] << "of size: " << file_size(repeated[i + 1]);
-			std::cin >> choice;
-			choice = std::tolower(choice);
-			switch (choice)
-			{
-			case 'l':
-				remove(repeated[i]);
-				break;
-			case 'r':
-				remove(repeated[i + 1]);
-				break;
-			default:
-				std::cout << "Wrong char, no image was deleted";
-				break;
-			}
-			std::cout << "\n";
-		}
+		manualDelete(); //Show the user every pair of images and make him choose
 		break;
 	default:
 		//Make a for to show all names
@@ -188,50 +207,38 @@ void hash_function(int start_point)
 	for (int i = start_point; i < image_path.size(); i += max_threads)
 	{
 		progress++;
-		try {
-			cv::UMat processing_image = cv::imread(image_path[i].string(), cv::IMREAD_COLOR).getUMat(cv::ACCESS_READ); //had to change from image_path[i].u8string() to .string() due to a library update
-			hasher = 1; //Must reset hasher to 1
-			if (processing_image.dims != 0)
+		//try {
+		cv::UMat processing_image = cv::imread(image_path[i].string(), cv::IMREAD_COLOR).getUMat(cv::ACCESS_READ); //had to change from image_path[i].u8string() to .string() due to a library update
+		hasher = 1; //Must reset hasher to 1
+		if (processing_image.dims != 0)
+		{
+			cv::UMat gray_image; //Define gray image to create gray scale images
+			cv::cvtColor(processing_image, gray_image, cv::COLOR_BGR2GRAY);
+			cv::resize(gray_image, gray_image, SIZE);
+			for (int j = 0; j < (gray_image.cols - 1); j++)
 			{
-				cv::UMat gray_image; //Define gray image to create gray scale images
-				cv::cvtColor(processing_image, gray_image, cv::COLOR_BGR2GRAY);
-				cv::resize(gray_image, gray_image, SIZE);
-				for (int j = 0; j < (gray_image.cols - 1); j++)
+				for (int l = 0; l < gray_image.rows; l++)
 				{
-					for (int l = 0; l < gray_image.rows; l++)
-					{
-						hasher = hasher + gray_image.getMat(cv::ACCESS_READ).at<uchar>(l, j); //will leave it like this for now, seems to work
-					}
-					hasher = hasher * hasher; //just to make hasher even more unique
+					hasher = hasher + gray_image.getMat(cv::ACCESS_READ).at<uchar>(l, j); //will leave it like this for now, seems to work
 				}
-				if (hashmap.empty() == true)
-				{
-					hashmap[hasher] = i;
-					//Add hash value to each path (path is a number of the position of a path in the vector image_path)
-				}
-				else
-				{
-					//Solved the counting error by simply skipping the already in check
-					it = hashmap.find(hasher);
-					if (it != hashmap.end())
-					{
-						repeated.push_back(image_path[it->second]);
-						repeated.push_back(image_path[i]);
-					}
-					else
-					{
-						//Add hash value to each path (path is a number of the position of a path in the vector image_path)
-						hashmap[hasher] = i;
-					}
-				}
+				hasher = hasher * hasher; //just to make hasher even more unique
+			}
+			it = hashmap.find(hasher);
+			if (it != hashmap.end())
+			{
+				repeated.push_back(image_path[it->second]);
+				repeated.push_back(image_path[i]);
 			}
 			else
 			{
-				hasher = -1;
+				//Add hash value to each path (path is a number of the position of a path in the vector image_path)
+				hashmap[hasher] = i;
 			}
 		}
+		/*}
 		catch (...) {
-		}
+
+		}*/
 	}
 }
 
@@ -251,14 +258,16 @@ int main()
 	char sure = 'z';
 	std::ifstream infile("config.cfg");
 	if (infile.fail()) {
-		std::cout << "Config file not found";
-		exit(0);
+		std::cout << "Config file not found!\n";
+		std::cout << "Press any button to leave";
+		std::cin >> options;
+		exit(-1);
 	}
 	std::string file_input;
-	std::cout << "Welcome to the Repeated Image Predator (RIP) v2.2\nThis program was created by Joan Gil (Linkedin: https://www.linkedin.com/in/joan-gil-rigo-a65536184/) \nand is used for free under the MIT license, check MIT info at: https://en.wikipedia.org/wiki/MIT_License \n";
+	std::cout << "Welcome to the Repeated Image Predator (RIP) v2.4\nThis program was created by Joan Gil (Linkedin: https://www.linkedin.com/in/joan-gil-rigo-a65536184/) \nand is used for free under the MIT license, check MIT info at: https://en.wikipedia.org/wiki/MIT_License \n";
 	std::cout << "Please think about donating: https://www.paypal.me/jgil99 \nFollow the instructions to begin search: \n\n";
 	//Oh no, not checking input again...
-	while (!is_directory(path))
+	do
 	{
 		std::cout << "Enter the path where you want to search: ";
 		std::getline(std::cin, path);
@@ -266,24 +275,24 @@ int main()
 		{
 			std::cout << "Not a valid directory, make sure it's not a path to a single image";
 		}
-	}
+	} while (!is_directory(path));
 	std::cout << "\nChoose between the different options: \n1 - Automatically delete images based on their size (saves heaviest)\n";
 	std::cout << "2 - Automatically delete images based on resolution (saves biggest resolution)\n";
 	std::cout << "3 - Manually delete images not recommended, takes a lot of time\n";
 	std::cout << "4 - Don't delete just show names of repeated images\n";
-	while (options < 1 || options > 4)
+	do
 	{
 		std::cout << "Enter number: ";
 		std::cin >> options;
 		if (options < 1 || options > 4) {
 			std::cout << "Invalid option, check if you used a number between 1 and 4\n";
 		}
-	}
+	} while (options < 1 || options > 4);
 	//add file reading check for opencl usage
-	std::getline(infile,file_input);
-	int initial_pos = file_input.find('=')+1;
-	int last_pos = file_input.find('#',initial_pos)-1;
-	file_input = file_input.substr(initial_pos, last_pos-initial_pos); //get correct part of string
+	std::getline(infile, file_input);
+	int initial_pos = file_input.find('=') + 1;
+	int last_pos = file_input.find('#', initial_pos) - 1;
+	file_input = file_input.substr(initial_pos, last_pos - initial_pos); //get correct part of string
 	if (file_input.compare("yes") == 0) {
 		cv::ocl::setUseOpenCL(true);
 		max_threads = 2; //Use only 2 threads if OpenCL is enabled, no need for more, no improvements
